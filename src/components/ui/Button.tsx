ff4c34954b02367903e905bf9a729be523a29a91
Satoki <satokiito@gmail.com>
2024-05-23 01:53:55 +0100

    feat: add interface for ButtonProps

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,5 @@
-import { cva } from 'class-variance-authority'
-import { FC } from 'react'
-
-interface ButtonProps {}
+import { cva, VariantProps } from 'class-variance-authority'
+import { ButtonHTMLAttributes, FC } from 'react'
 
 const buttonVariants = cva(
     'active: scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
@@ -23,6 +21,11 @@ const buttonVariants = cva(
         }
     }
 )
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+    isLoading?: boolean
+}
+
 
 const button: FC<ButtonProps> = ({}) => {
   return <div>Button</div>
