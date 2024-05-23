8a290761895e03cb82768d871750e3c3512f5a9e
Satoki <satokiito@gmail.com>
2024-05-23 18:38:27 +0100

    feat: file organisation. create page for adding friend

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const page: FC = ({}) => {
+  return (
+    <main className="pt-8">
+      <h1 className="font-bold text-5xl mb-8">Add Friend</h1>
+      <AddFriendBtn />
+    </main>
+  );
+};
+
+export default page;
