8e82e61edabb757c9fa211f012904bc8d0f31f90
Satoki <satokiito@gmail.com>
2024-05-23 03:37:18 +0100

    feat: update styling of toaster

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,19 +1,49 @@
-"use client"
+"use client";
 
-import { FC, ReactNode } from 'react'
-import { Toaster } from 'react-hot-toast'
+import { FC, ReactNode } from "react";
+import { toast, Toaster, ToastBar } from "react-hot-toast";
 
 interface ProvidersProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 const Providers: FC<ProvidersProps> = ({ children }) => {
   return (
     <>
-        <Toaster position='top-center' reverseOrder={false} />
-        {children}
+      <Toaster
+        toastOptions={{
+          style: {
+            border: "1px solid #713200",
+            fontSize: "14px",
+            padding: "10px 20px",
+            textAlign: "center"
+          },
+        }}
+        position="top-center"
+        reverseOrder={false}
+      >
+        {(t) => (
+          <ToastBar toast={t}>
+            {({ icon, message }) => (
+              <>
+                {icon}
+                {message}
+                {t.type !== "loading" && (
+                  <button
+                    className="border border-transparent rounded-none rounded-r-lg flex items-end text-sm font-medium text-red-500 hover:text-red-300 "
+                    onClick={() => toast.dismiss(t.id)}
+                  >
+                    Dismiss
+                  </button>
+                )}
+              </>
+            )}
+          </ToastBar>
+        )}
+      </Toaster>
+      {children}
     </>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;
