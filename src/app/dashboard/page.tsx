8507545c721c977c60ce5975102b6a0f69c313c3
Satoki <satokiito@gmail.com>
2024-05-23 18:29:21 +0100

    feat: add signout button and session to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,20 @@
+import SignOutBtn from '@/components/SignOutBtn'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 import { FC } from 'react'
 
 interface PageProps {
   
 }
 
-const Page: FC<PageProps> = ({}) => {
-  return <div>Dashboard Page</div>
+const Page = async ({}) => {
+    const session = await getServerSession(authOptions)
+  return (
+    <>
+        <div>Dashboard Page</div>
+        <SignOutBtn />
+    </>
+  ) 
 }
 
 export default Page
