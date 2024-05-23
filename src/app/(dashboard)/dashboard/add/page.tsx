0c0e8af855b7af379c8f9a13eb7d7b600fa01da7
Satoki <satokiito@gmail.com>
2024-05-23 18:48:17 +0100

    feat: import AddFriendForm component into add friend page

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,10 +1,11 @@
+import AddFriendForm from "@/components/AddFriendForm";
 import { FC } from "react";
 
 const page: FC = ({}) => {
   return (
     <main className="pt-8">
       <h1 className="font-bold text-5xl mb-8">Add Friend</h1>
-      <AddFriendBtn />
+      <AddFriendForm />
     </main>
   );
 };
