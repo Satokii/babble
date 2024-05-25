2e2107bd44ab212a6f3037cd8d401500caaec2dd
Satoki <satokiito@gmail.com>
2024-05-25 17:04:42 +0100

    feat: add base code for add friend api route

--- src/components/AddFriendForm.tsx
+++ src/components/AddFriendForm.tsx
@@ -23,7 +23,7 @@ const AddFriendForm: FC<AddFriendFormProps> = ({}) => {
     try {
       const validatedEmail = addFriendValidator.parse({ email });
 
-      await axios.post("/api/friends/add", {
+      await axios.post("/api/friend/add", {
         email: validatedEmail,
       });
 
