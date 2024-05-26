9d7e772e5af654c6841d81f4fce603b00b8a5ac3
Satoki <satokiito@gmail.com>
2024-05-26 20:36:17 +0100

    feat: add initial divs to dashboard sidebar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,21 +1,29 @@
 import { authOptions } from "@/lib/auth";
+import { Link } from "lucide-react";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
 
 interface LayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const Layout = async ({ children }: LayoutProps) => {
-    
-    const session = await getServerSession(authOptions)
-    
-    if (!session) {
-        notFound()
-    }
-    
-    return <div>{children}</div>
-}
+  const session = await getServerSession(authOptions);
 
-export default Layout
+  if (!session) {
+    notFound();
+  }
+
+  return (
+    <div className="w-full flex h-screen">
+      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6"></div>
+      <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
+        
+      </Link>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
