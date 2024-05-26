a3caab4664a56991e1e4c543eee68844c2cb5086
Satoki <satokiito@gmail.com>
2024-05-26 21:26:45 +0100

    feat: map through sidebar options to render to sidebar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { Icon, Icons } from "@/components/Icons";
 import { authOptions } from "@/lib/auth";
-import { Link } from "lucide-react";
+import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
@@ -10,11 +10,20 @@ interface LayoutProps {
 }
 
 interface SidebarOption {
-    id: number
-    name: string
-    href: string
-    icon: Icon
+  id: number;
+  name: string;
+  href: string;
+  icon: Icon;
 }
+
+const sidebarOptions: SidebarOption[] = [
+  {
+    id: 1,
+    name: "Add Friend",
+    href: "/dashboard/add",
+    icon: "UserPlus",
+  },
+];
 
 const Layout = async ({ children }: LayoutProps) => {
   const session = await getServerSession(authOptions);
@@ -27,7 +36,7 @@ const Layout = async ({ children }: LayoutProps) => {
     <div className="w-full flex h-screen">
       <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
-          <Icons.Logo className="w-30"></Icons.Logo>
+          <Icons.Logo className=" h-6 w-auto" />
         </Link>
 
         <div className="text-xs font-semibold leading-6 text-gray-400">
@@ -41,7 +50,25 @@ const Layout = async ({ children }: LayoutProps) => {
               <div className="text-xs font-semibold leading-6 text-gray-400">
                 Overview
               </div>
-              <ul role="list" className="-mx-2 mt-2 space-y-1"></ul>
+              <ul role="list" className="-mx-2 mt-2 space-y-1">
+                {sidebarOptions.map((option) => {
+                  const Icon = Icons[option.icon];
+                  return (
+                    <li key={option.id}>
+                      <Link
+                        href={option.href}
+                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                      >
+                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                            <Icon className="h-4 w-4" />
+                        </span>
+                        
+                        <span className="truncate">{option.name}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </li>
           </ul>
         </nav>
