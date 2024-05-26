b66189729df1a2166fdc10e50d0d481c1829ae34
Satoki <satokiito@gmail.com>
2024-05-26 20:29:17 +0100

    feat: add get session in dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,10 +1,20 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
 
 interface LayoutProps {
     children: ReactNode
 }
 
-const Layout: FC<LayoutProps> = ({ children }) => {
+const Layout = async ({ children }: LayoutProps) => {
+    
+    const session = await getServerSession(authOptions)
+    
+    if (!session) {
+        notFound()
+    }
+    
     return <div>{children}</div>
 }
 
