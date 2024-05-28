915e672e5d23f0029d4d407c381bcaf5f7b70332
Satoki <satokiito@gmail.com>
2024-05-28 22:03:19 +0100

    feat: create new component FriendRequestMenu

---
+++ src/components/FriendRequestMenu.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface FriendRequestMenuProps {
+  
+}
+
+const FriendRequestMenu: FC<FriendRequestMenuProps> = ({}) => {
+  return <div>FriendRequestMenu</div>
+}
+
+export default FriendRequestMenu
