b46aec5314bd4f6b9c8b6b4b40a81305e0cc3145
Satoki <satokiito@gmail.com>
2024-05-28 23:06:20 +0100

    feat: add fetch request to get user friend request ids

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -1,14 +1,20 @@
-import { authOptions } from '@/lib/auth'
-import { getServerSession } from 'next-auth'
-import { notFound } from 'next/navigation'
-import { FC } from 'react'
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
 
-const page: FC = ({}) => {
-    const session = getServerSession(authOptions)
-    if (!session) {
-        notFound()
-    }
-  return <div>page</div>
-}
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    notFound();
+  }
 
-export default page
+  const userFriendReqIds = (await fetchRedis(
+    "smembers",
+    `user:${session.user.id}:incoming_friend_requests`
+  )) as string[];
+
+  return <div>page</div>;
+};
+
+export default page;
