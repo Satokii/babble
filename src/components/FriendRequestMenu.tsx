4826349c4753a2e55a3980e4208330087c9c551d
Satoki <satokiito@gmail.com>
2024-05-28 22:10:17 +0100

    feat: add content to FriendRequestMenu componenet

--- src/components/FriendRequestMenu.tsx
+++ src/components/FriendRequestMenu.tsx
@@ -1,3 +1,5 @@
+import { User } from 'lucide-react'
+import Link from 'next/link'
 import { FC } from 'react'
 
 interface FriendRequestMenuProps {
@@ -5,7 +7,11 @@ interface FriendRequestMenuProps {
 }
 
 const FriendRequestMenu: FC<FriendRequestMenuProps> = ({}) => {
-  return <div>FriendRequestMenu</div>
+  return <Link href="/dashboard/friend-requests" className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'>
+    <div className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
+        <User className="h-4 w-4" />
+    </div>
+  </Link>
 }
 
 export default FriendRequestMenu
