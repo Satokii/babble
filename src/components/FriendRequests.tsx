69008358f224d7f0aa98cd7e3350d9ded45ed8c3
Satoki <satokiito@gmail.com>
2024-05-29 19:19:27 +0100

    feat: add onClick to accept/deny buttons to handle requests

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -20,7 +20,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   );
 
   const acceptFriendReq = async (senderId: string) => {
-    await axios.post("/api/requests/accept", { id: senderId })
+    await axios.post("/api/friend/accept", { id: senderId })
 
     setFriendRequests((allRequests) => allRequests.filter((request) => request.senderId !== senderId))
 
@@ -28,7 +28,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   }
 
   const rejectFriendReq = async (senderId: string) => {
-    await axios.post("/api/requests/deny", { id: senderId })
+    await axios.post("/api/friend/deny", { id: senderId })
 
     setFriendRequests((allRequests) => allRequests.filter((request) => request.senderId !== senderId))
 
@@ -44,10 +44,10 @@ const FriendRequests: FC<FriendRequestsProps> = ({
           <div key={request.senderId} className="flex gap-4 items-center">
             <UserPlus className="text-black" />
             <p className="font-medium text-lg">{request.senderEmail}</p>
-            <button aria-label="accept friend" className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md">
+            <button aria-label="accept friend" className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md" onClick={() => acceptFriendReq(request.senderId)}>
               <Check className="font-semibold text-white w-3/4 h-3/4" />
             </button>
-            <button aria-label="deny friend" className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md">
+            <button aria-label="deny friend" className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md" onClick={() => rejectFriendReq(request.senderId)}>
               <X className="font-semibold text-white w-3/4 h-3/4" />
             </button>
           </div>
