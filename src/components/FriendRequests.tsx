f5550c4604beca0e1a3612328480f564a5197cef
Satoki <satokiito@gmail.com>
2024-05-29 17:23:02 +0100

    feat: add reject button to friend requests

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Check, UserPlus } from "lucide-react";
+import { Check, UserPlus, X } from "lucide-react";
 import { FC, useState } from "react";
 
 interface FriendRequestsProps {
@@ -27,7 +27,9 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <button aria-label="accept friend" className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md">
               <Check className="font-semibold text-white w-3/4 h-3/4" />
             </button>
-            <button></button>
+            <button aria-label="deny friend" className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md">
+              <X className="font-semibold text-white w-3/4 h-3/4" />
+            </button>
           </div>
         ))
       )}
