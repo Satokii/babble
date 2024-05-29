a3206f1e220c99a8c5f563290c492fa0f6117db8
Satoki <satokiito@gmail.com>
2024-05-29 19:13:23 +0100

    feat: add api endpoint for accepting friend request

---
+++ src/app/api/friend/accept/route.ts
@@ -0,0 +1,34 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const isAlreadyFriend = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`
+    );
+
+    if (isAlreadyFriend) {
+      return new Response("You are already friends with this user.", {
+        status: 400,
+      });
+    }
+
+    const hasFriendRequest = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd
+    );
+  } catch (err) {}
+}
