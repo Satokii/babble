03c1a0600a48ca9a1e2dfa3b080e946c3318e6d1
Satoki <satokiito@gmail.com>
2024-05-29 17:57:00 +0100

    fix: update fetch request to get user details from db due to caching error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,50 +1,77 @@
 import { NextAuthOptions } from "next-auth";
 import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import { db } from "./db";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
+import { fetchRedis } from "@/helpers/redis";
 
 export const authOptions: NextAuthOptions = {
-    adapter: UpstashRedisAdapter(db),
-    session: {
-        strategy: 'jwt'
-    },
-    pages: {
-        signIn: '/login'
-    },
-    providers: [
-        GoogleProvider({
-            clientId: `${process.env.GOOGLE_CLIENT_ID}`,
-            clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.id = user.id;
-            } else {
-                const dbUser = await db.get(`user:${token.id}`) as User | null;
-                if (dbUser) {
-                    token.id = dbUser.id;
-                    token.name = dbUser.name;
-                    token.email = dbUser.email;
-                    token.picture = dbUser.image;
-                }
-            }
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: `${process.env.GOOGLE_CLIENT_ID}`,
+      clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+    //   if (user) {
+    //     token.id = user.id;
+    //   } else {
+    //     const dbUser = (await fetchRedis("get", `user:${token.id}`)) as
+    //       | string
+    //       | null;
+    //     if (dbUser) {
+    //       token.id = dbUser.id;
+    //       token.name = dbUser.name;
+    //       token.email = dbUser.email;
+    //       token.picture = dbUser.image;
+    //     }
+    //   }
+        const dbUserResult = (await fetchRedis("get", `user:${token.id}`)) as
+          | string
+          | null;
 
-            return token;
-        },
-        async session({ session, token }) {
-            if (token) {
-                session.user.id = token.id
-                session.user.name = token.name
-                session.user.email = token.email
-                session.user.image = token.picture
+          if (!dbUserResult) {
+            if (user) {
+                token.id = user!.id
             }
-            return session
-        },
-        redirect() {
-            return '/dashboard'
-        }
-    }
-}
+            return token
+          }
+
+          const dbUser = JSON.parse(dbUserResult) as User
+
+        //   console.log("dbUser", {
+        //     id: dbUser.id,
+        //   name: dbUser.name,
+        //   email: dbUser.email,
+        //   picture: dbUser.image
+        //   })
+
+        return {
+          id: dbUser.id,
+          name: dbUser.name,
+          email: dbUser.email,
+          picture: dbUser.image
+      }
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+    redirect() {
+      return "/dashboard";
+    },
+  },
+};
