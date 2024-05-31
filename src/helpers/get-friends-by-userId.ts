37edea4d4478e7776c4c5b8e6b5c54a8b91696dd
Satoki <satokiito@gmail.com>
2024-05-31 14:36:24 +0100

    refactor: format getFriendsByUserId function

--- src/helpers/get-friends-by-userId.ts
+++ src/helpers/get-friends-by-userId.ts
@@ -1,14 +1,17 @@
-import { fetchRedis } from "./redis"
+import { fetchRedis } from "./redis";
 
 export const getFriendsByUserId = async (userId: string) => {
-    const friendIds = await fetchRedis("smembers", `user:${userId}:friends`) as string[]
+  const friendIds = (await fetchRedis(
+    "smembers",
+    `user:${userId}:friends`
+  )) as string[];
 
-    const friends = await Promise.all(
-        friendIds.map(async (friendId) => {
-            const friend = await fetchRedis("get", `user${friendId}`) as User
-            return friend
-        })
-    )
+  const friends = await Promise.all(
+    friendIds.map(async (friendId) => {
+      const friend = (await fetchRedis("get", `user${friendId}`)) as User;
+      return friend;
+    })
+  );
 
-    return friends
-}
+  return friends;
+};
