21993ecd40e65b5fca0742c3821ca5e23a6c032b
Satoki <satokiito@gmail.com>
2024-05-31 15:21:18 +0100

    feat: import SidebarChatList component into dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -9,6 +9,7 @@ import SignOutBtn from "@/components/SignOutBtn";
 import FriendRequestMenu from "@/components/FriendRequestMenu";
 import { fetchRedis } from "@/helpers/redis";
 import { getFriendsByUserId } from "@/helpers/get-friends-by-userId";
+import SidebarChatList from "@/components/SidebarChatList";
 
 interface LayoutProps {
   children: ReactNode;
@@ -61,7 +62,9 @@ const Layout = async ({ children }: LayoutProps) => {
 
         <nav className="flex flex-1 flex-col">
           <ul role="list" className="flex flex-1 flex-col gap-y-7">
-            <li>Example chat HiHiHi</li>
+            <li>
+              <SidebarChatList friends={friends} sessionId={session.user.id} />
+            </li>
             <li>
               <div className="text-xs font-semibold leading-6 text-gray-400">
                 Overview
