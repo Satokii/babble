bd9d0751c0e9900de303f991a25ebe2b9b262051
Satoki <satokiito@gmail.com>
2024-05-31 13:48:42 +0100

    feat: use params to determine chat page and pass as props

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,11 +1,13 @@
-import { FC } from 'react'
+import { FC } from "react";
 
-interface pageProps {
-  
+interface PageProps {
+  params: {
+    chatId: string
+  }
 }
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>
-}
+const page: FC<PageProps> = ({ params }: PageProps) => {
+  return <div>{params.chatId}</div>;
+};
 
-export default page
+export default page;
