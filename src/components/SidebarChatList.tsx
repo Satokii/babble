3f573fce4f43226aac9fda0e56e7f52218d6d1a2
Satoki <satokiito@gmail.com>
2024-05-31 14:59:09 +0100

    feat: add useEffect to check and save unread messages to state

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,15 +1,30 @@
-import { FC, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
 
 interface SidebarChatListProps {
   friends: User[];
 }
 
 const SidebarChatList: FC<SidebarChatListProps> = ({ friends }) => {
+  const router = useRouter();
+  const pathname = usePathname();
   const [unreadMessages, setUnreadMessages] = useState<Message[]>([]);
 
+  useEffect(() => {
+    if (pathname?.includes("chat")) {
+      setUnreadMessages((prevMessages) => {
+        return prevMessages.filter(
+          (message) => !pathname.includes(message.senderId)
+        );
+      });
+    }
+  }, [pathname]);
+
   return (
-    <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
-    </ul>
+    <ul
+      role="list"
+      className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1"
+    ></ul>
   );
 };
 
