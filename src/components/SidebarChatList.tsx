b85bbd22fabbe7e19909ce826eac8ce770d68c77
Satoki <satokiito@gmail.com>
2024-05-31 14:43:47 +0100

    feat: initialise SidebarChatList component

---
+++ src/components/SidebarChatList.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface SidebarChatListProps {}
+
+const SidebarChatList: FC<SidebarChatListProps> = ({}) => {
+  return <div>SidebarChatList</div>;
+};
+
+export default SidebarChatList;
