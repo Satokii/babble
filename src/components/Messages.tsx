340fc9b3db85562e9dff3f50cf08b6d0a0454804
Satoki <satokiito@gmail.com>
2024-05-31 16:09:41 +0100

    feat: add initial div and styling to Messages component

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,11 +1,13 @@
-import { FC } from 'react'
+import { FC } from "react";
 
-interface MessagesProps {
-  
-}
+interface MessagesProps {}
 
 const Messages: FC<MessagesProps> = ({}) => {
-  return <div>Messages</div>
-}
+  return (
+    <div id="messages" className="flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+      Messages
+    </div>
+  );
+};
 
-export default Messages
+export default Messages;
