21f4d457fbeb7e2b24e7eee39456f32fb223f83c
Satoki <satokiito@gmail.com>
2024-05-31 14:07:12 +0100

    feat: add function to get all chat messages

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,3 +1,4 @@
+import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { getServerSession } from "next-auth";
@@ -5,27 +6,45 @@ import { notFound } from "next/navigation";
 
 interface PageProps {
   params: {
-    chatId: string
+    chatId: string;
+  };
+}
+
+async function getChatMessages(chatId: string) {
+  try {
+    const messages: string[] = await fetchRedis(
+      "zrange",
+      `chat:${chatId}:messages`,
+      0,
+      -1
+    );
+
+  const dbMessages = messages.map((message) => JSON.parse(message) as Message)
+
+  } catch (err) {
+    notFound();
   }
 }
 
 const page = async ({ params }: PageProps) => {
-  const { chatId } = params
-  const session = await getServerSession(authOptions)
+  const { chatId } = params;
+  const session = await getServerSession(authOptions);
 
   if (!session) {
-    notFound()
+    notFound();
   }
 
-  const { user } = session
-  const [userId1, userId2] = chatId.split("--")
+  const { user } = session;
+  const [userId1, userId2] = chatId.split("--");
 
   if (user.id !== userId1 && user.id !== userId2) {
-    notFound()
+    notFound();
   }
 
-  const chatFriendId = user.id === userId1 ? userId2 : userId1
-  const chatFriend = await db.get(`user:${chatFriendId}`) as User
+  const chatFriendId = user.id === userId1 ? userId2 : userId1;
+  const chatFriend = (await db.get(`user:${chatFriendId}`)) as User;
+
+  const initialMessages = await getChatMessages(chatId);
 
   return <div>{params.chatId}</div>;
 };
