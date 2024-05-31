5d04258ef7dad742c9326d2c1babfcbe7b683136
Satoki <satokiito@gmail.com>
2024-05-31 14:52:45 +0100

    feat: add state to store unread messages

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,9 +1,16 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
-interface SidebarChatListProps {}
+interface SidebarChatListProps {
+  friends: User[];
+}
 
-const SidebarChatList: FC<SidebarChatListProps> = ({}) => {
-  return <div>SidebarChatList</div>;
+const SidebarChatList: FC<SidebarChatListProps> = ({ friends }) => {
+  const [unreadMessages, setUnreadMessages] = useState<Message[]>([]);
+
+  return (
+    <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
+    </ul>
+  );
 };
 
 export default SidebarChatList;
