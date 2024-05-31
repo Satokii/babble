0527503bc2dec6b47517266f6e56f03bd30961d1
Satoki <satokiito@gmail.com>
2024-05-31 16:27:16 +0100

    feat: map through messages to check current user and if subsequent messages

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -5,9 +5,10 @@ import { FC, useRef, useState } from "react";
 
 interface MessagesProps {
   existingMessages: Message[];
+  sessionId: string;
 }
 
-const Messages: FC<MessagesProps> = ({ existingMessages }) => {
+const Messages: FC<MessagesProps> = ({ existingMessages, sessionId }) => {
   const [messages, setMessages] = useState<Message[]>(existingMessages);
   const scrollDownRef = useRef<HTMLDivElement | null>(null);
   return (
@@ -16,6 +17,13 @@ const Messages: FC<MessagesProps> = ({ existingMessages }) => {
       className="flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
     >
       <div ref={scrollDownRef} />
+      {messages.map((message, index) => {
+        const isCurrentUser = message.senderId === sessionId;
+
+        const subsequentUserMessages =
+          messages[index - 1]?.senderId === messages[index].senderId;
+
+      })}
     </div>
   );
 };
