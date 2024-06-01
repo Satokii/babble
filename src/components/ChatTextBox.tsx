eda77507b4a3a963d1e30ba12b32724170a7db4b
Satoki <satokiito@gmail.com>
2024-06-01 16:19:11 +0100

    feat: import TextareaAutosize into ChatTextbox component

--- src/components/ChatTextBox.tsx
+++ src/components/ChatTextBox.tsx
@@ -1,15 +1,20 @@
-import { FC } from 'react'
+"use client"
 
-interface ChatTextBoxProps {
-  
-}
+import { FC, useRef } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+
+interface ChatTextBoxProps {}
 
 const ChatTextBox: FC<ChatTextBoxProps> = ({}) => {
-  return <div className='border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0'>
-    <div className='relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600'>
-        
-    </div>
-  </div>
-}
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
-export default ChatTextBox
+  return (
+    <div className="border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+        <TextareaAutosize ref={textareaRef} />
+      </div>
+    </div>
+  );
+};
+
+export default ChatTextBox;
