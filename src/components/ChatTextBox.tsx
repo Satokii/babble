54a77505296a6825df4b907620a02917dafee0e3
Satoki <satokiito@gmail.com>
2024-06-01 16:33:21 +0100

    feat: add content to TextareaAutosize component

--- src/components/ChatTextBox.tsx
+++ src/components/ChatTextBox.tsx
@@ -4,7 +4,7 @@ import { FC, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 interface ChatTextBoxProps {
-  chatFriend: User;
+    chatFriend: User
 }
 
 const ChatTextBox: FC<ChatTextBoxProps> = ({ chatFriend }) => {
@@ -15,7 +15,20 @@ const ChatTextBox: FC<ChatTextBoxProps> = ({ chatFriend }) => {
   return (
     <div className="border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
       <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
-        <TextareaAutosize ref={textareaRef} />
+        <TextareaAutosize
+          className="block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm sm:leading-6"
+          ref={textareaRef}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              sendMessage();
+            }
+          }}
+          rows={1}
+          value={messageContent}
+          onChange={(e) => setMessageContent(e.target.value)}
+          placeholder={`Send a message to ${chatFriend.name}`}
+        />
       </div>
     </div>
   );
