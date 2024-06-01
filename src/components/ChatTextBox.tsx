a5669149296be6b37f8eb0b37493c6871b551fd4
Satoki <satokiito@gmail.com>
2024-06-01 16:03:45 +0100

    feat: initialise ChatTextBox componenet file

---
+++ src/components/ChatTextBox.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ChatTextBoxProps {
+  
+}
+
+const ChatTextBox: FC<ChatTextBoxProps> = ({}) => {
+  return <div>ChatTextBox</div>
+}
+
+export default ChatTextBox
