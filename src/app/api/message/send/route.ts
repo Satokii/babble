bae71e729d5d464ae5e7245920ea8267fd2ea27d
Satoki <satokiito@gmail.com>
2024-06-01 16:56:54 +0100

    feat: add error handling to check user is authorised to send messages in a certain chat

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -1,3 +1,21 @@
-export async function POST() {
-    
+import { authOptions } from "@/lib/auth"
+import { getServerSession } from "next-auth"
+
+export async function POST(req: Request) {
+    try {
+        const { text, chatId } = await req.json()
+        const session = await getServerSession(authOptions)
+
+        if (!session) {
+            return new Response("Unauthorized", { status: 401 })
+        }
+
+        const [userId1, userId2] = chatId.split("--")
+
+        if (session.user.id !== userId1 && session.user.id !== userId2) {
+            return new Response("Unauthorized", { status: 401 })
+        }
+    } catch (error) {
+        
+    }
 }
