1e3056f5fd34087bd8dab71702232c46398cb165
Satoki <satokiito@gmail.com>
2024-06-02 14:58:23 +0100

    feat: create new pusherClient variable

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,4 +1,5 @@
 import PusherServer from "pusher";
+import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
@@ -7,3 +8,10 @@ export const pusherServer = new PusherServer({
   cluster: "eu",
   useTLS: true,
 });
+
+export const pusherClient = new PusherClient(
+    process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    {
+        cluster: "eu"
+    }
+)
