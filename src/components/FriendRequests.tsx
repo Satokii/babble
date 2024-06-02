3c92065bc13f9c9dda027007051ddf6a87c92c8f
Satoki <satokiito@gmail.com>
2024-06-02 15:16:48 +0100

    feat: create useEffect to listen for any friend requests made to a user

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { pusherClient } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import axios from "axios";
 import { Check, UserPlus, X } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface FriendRequestsProps {
   incomingFriendRequests: IncomingFriendRequest[];
@@ -14,26 +16,47 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   incomingFriendRequests,
   sessionId,
 }) => {
-  const router = useRouter()
+  const router = useRouter();
   const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests
   );
 
   const acceptFriendReq = async (senderId: string) => {
-    await axios.post("/api/friend/accept", { id: senderId })
+    await axios.post("/api/friend/accept", { id: senderId });
 
-    setFriendRequests((allRequests) => allRequests.filter((request) => request.senderId !== senderId))
+    setFriendRequests((allRequests) =>
+      allRequests.filter((request) => request.senderId !== senderId)
+    );
 
-    router.refresh()
-  }
+    router.refresh();
+  };
 
   const rejectFriendReq = async (senderId: string) => {
-    await axios.post("/api/friend/reject", { id: senderId })
+    await axios.post("/api/friend/reject", { id: senderId });
 
-    setFriendRequests((allRequests) => allRequests.filter((request) => request.senderId !== senderId))
+    setFriendRequests((allRequests) =>
+      allRequests.filter((request) => request.senderId !== senderId)
+    );
 
-    router.refresh()
-  }
+    router.refresh();
+  };
+
+  useEffect(() => {
+    pusherClient.subscribe(
+      toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+    );
+
+    const friendRequestHandler = () => {};
+
+    pusherClient.bind("incoming_friend_requests", friendRequestHandler);
+
+    return () => {
+      pusherClient.unsubscribe(
+        toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+      );
+      pusherClient.unbind("incoming_friend_requests", friendRequestHandler);
+    };
+  }, [sessionId]);
 
   return (
     <>
@@ -44,10 +67,18 @@ const FriendRequests: FC<FriendRequestsProps> = ({
           <div key={request.senderId} className="flex gap-4 items-center">
             <UserPlus className="text-black" />
             <p className="font-medium text-lg">{request.senderEmail}</p>
-            <button aria-label="accept friend" className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md" onClick={() => acceptFriendReq(request.senderId)}>
+            <button
+              aria-label="accept friend"
+              className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
+              onClick={() => acceptFriendReq(request.senderId)}
+            >
               <Check className="font-semibold text-white w-3/4 h-3/4" />
             </button>
-            <button aria-label="deny friend" className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md" onClick={() => rejectFriendReq(request.senderId)}>
+            <button
+              aria-label="deny friend"
+              className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
+              onClick={() => rejectFriendReq(request.senderId)}
+            >
               <X className="font-semibold text-white w-3/4 h-3/4" />
             </button>
           </div>
