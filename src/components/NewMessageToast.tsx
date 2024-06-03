ec45415585075e9e18a4274a4f741a5b038a7838
Satoki <satokiito@gmail.com>
2024-06-03 17:59:48 +0100

    feat: initiate NewMessageToast component

---
+++ src/components/NewMessageToast.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface NewMessageToastProps {}
+
+const NewMessageToast: FC<NewMessageToastProps> = ({}) => {
+  return <div>NewMessageToast</div>;
+};
+
+export default NewMessageToast;
