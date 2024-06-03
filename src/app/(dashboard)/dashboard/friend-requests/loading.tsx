a98aac71c33788c1932e95c6e19b1bc475b40a95
Satoki <satokiito@gmail.com>
2024-06-03 18:52:55 +0100

    feat: add loading skeletons to friend requests page

--- src/app/(dashboard)/dashboard/friend-requests/loading.tsx
+++ src/app/(dashboard)/dashboard/friend-requests/loading.tsx
@@ -1,11 +1,18 @@
-import { FC } from 'react'
+import { FC } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-interface loadingProps {
-  
-}
+interface loadingProps {}
 
 const loading: FC<loadingProps> = ({}) => {
-  return <div>loading</div>
-}
+  return (
+    <div className="w-full flex flex-col gap-3">
+      <Skeleton className="mb-4" height={60} width={500} />
+      <Skeleton height={50} width={350} />
+      <Skeleton height={50} width={350} />
+      <Skeleton height={50} width={350} />
+    </div>
+  );
+};
 
-export default loading
+export default loading;
