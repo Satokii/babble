e98a266c287edf90cf126b84b0cbd91f7dbdb837
Satoki <satokiito@gmail.com>
2024-06-04 19:31:46 +0100

    map through sidebarOptions to render in MobileLayout

--- src/components/MobileLayout.tsx
+++ src/components/MobileLayout.tsx
@@ -18,9 +18,10 @@ import { Session } from "next-auth";
 interface MobileLayoutProps {
   friends: User[];
   session: Session;
+  sidebarOptions: SidebarOption[]
 }
 
-const MobileLayout: FC<MobileLayoutProps> = ({ friends, session }) => {
+const MobileLayout: FC<MobileLayoutProps> = ({ friends, session, sidebarOptions }) => {
   const [open, setOpen] = useState<boolean>(false);
   return (
     <div className="fixed bg-zinc-50 border-b border-zinc-200 top-0 inset-x-0 py-2 px-4">
@@ -109,6 +110,26 @@ const MobileLayout: FC<MobileLayoutProps> = ({ friends, session }) => {
                               <div className="text-xs font-semibold leading-6 text-gray-400">
                                 Overview
                               </div>
+                              <ul role="list" className="-mx-2 mt-2 space-y-1">
+                                {sidebarOptions.map((option) => {
+                                  const Icon = Icons[option.Icon];
+                                  return (
+                                    <li key={option.name}>
+                                      <Link
+                                        href={option.href}
+                                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                      >
+                                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                                          <Icon className="h-4 w-4" />
+                                        </span>
+                                        <span className="truncate">
+                                          {option.name}
+                                        </span>
+                                      </Link>
+                                    </li>
+                                  );
+                                })}
+                              </ul>
                             </li>
                           </ul>
                         </nav>
