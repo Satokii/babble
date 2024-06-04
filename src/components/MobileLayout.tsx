acc8aedc7bc454e0b3c6b0a2ac914c2687ae942a
Satoki <satokiito@gmail.com>
2024-06-04 18:52:00 +0100

    feat: add a top level div to MobileLayout component

--- src/components/MobileLayout.tsx
+++ src/components/MobileLayout.tsx
@@ -15,68 +15,70 @@ interface MobileLayoutProps {}
 const MobileLayout: FC<MobileLayoutProps> = ({}) => {
   const [open, setOpen] = useState<boolean>(false);
   return (
-    <Transition show={open} as={Fragment}>
-      <Dialog className="relative z-10" onClose={setOpen}>
-        <TransitionChild
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </TransitionChild>
+    <div className="fixed bg-zinc-50 border-b border-zinc-200 top-0 inset-x-0 py-2px4">
+      <Transition show={open} as={Fragment}>
+        <Dialog className="relative z-10" onClose={setOpen}>
+          <TransitionChild
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </TransitionChild>
 
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-              <TransitionChild
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <DialogPanel className="pointer-events-auto relative w-screen max-w-md">
-                  <TransitionChild
-                    enter="ease-in-out duration-500"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-500"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
-                      <button
-                        type="button"
-                        className="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                        onClick={() => setOpen(false)}
-                      >
-                        <span className="absolute -inset-2.5" />
-                        <span className="sr-only">Close panel</span>
-                        <X className="h-6 w-6" aria-hidden="true" />
-                      </button>
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <TransitionChild
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <DialogPanel className="pointer-events-auto relative w-screen max-w-md">
+                    <TransitionChild
+                      enter="ease-in-out duration-500"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in-out duration-500"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
+                        <button
+                          type="button"
+                          className="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                          onClick={() => setOpen(false)}
+                        >
+                          <span className="absolute -inset-2.5" />
+                          <span className="sr-only">Close panel</span>
+                          <X className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </TransitionChild>
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                      <div className="px-4 sm:px-6">
+                        <DialogTitle className="text-base font-semibold leading-6 text-gray-900">
+                          Panel title
+                        </DialogTitle>
+                      </div>
+                      <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                        {/* Your content */}
+                      </div>
                     </div>
-                  </TransitionChild>
-                  <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                    <div className="px-4 sm:px-6">
-                      <DialogTitle className="text-base font-semibold leading-6 text-gray-900">
-                        Panel title
-                      </DialogTitle>
-                    </div>
-                    <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                      {/* Your content */}
-                    </div>
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
+                  </DialogPanel>
+                </TransitionChild>
+              </div>
             </div>
           </div>
-        </div>
-      </Dialog>
-    </Transition>
+        </Dialog>
+      </Transition>
+    </div>
   );
 };
 
