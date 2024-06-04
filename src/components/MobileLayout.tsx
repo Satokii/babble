5644692e49c5516f45dd1675188f36f2e7519df1
Satoki <satokiito@gmail.com>
2024-06-04 18:39:29 +0100

    feat: initialise MobileLayout component

---
+++ src/components/MobileLayout.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { FC } from "react";
+
+interface MobileLayoutProps {}
+
+const MobileLayout: FC<MobileLayoutProps> = ({}) => {
+  return <div>MobileLayout</div>;
+};
+
+export default MobileLayout;
