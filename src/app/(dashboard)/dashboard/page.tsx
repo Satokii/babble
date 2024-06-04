07125dcdae7f18d8a4d01645d62449a1cf535c38
Satoki <satokiito@gmail.com>
2024-06-04 17:38:46 +0100

    feat: get friends from db as assign to variable in dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,27 +1,30 @@
-import SignOutBtn from '@/components/SignOutBtn'
-import { authOptions } from '@/lib/auth'
-import { db } from '@/lib/db'
-import { getServerSession } from 'next-auth'
-import { notFound } from 'next/navigation'
-import { FC } from 'react'
+import SignOutBtn from "@/components/SignOutBtn";
+import { getFriendsByUserId } from "@/helpers/get-friends-by-userId";
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { chatHrefConstructor } from "@/lib/utils";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import { FC } from "react";
 
-interface PageProps {
-  
-}
+interface PageProps {}
 
 const Page = async ({}) => {
-    const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-    if (!session) {
-      return notFound()
-    }
-    
+  if (!session) {
+    return notFound();
+  }
+
+  const friends = await getFriendsByUserId(session.user.id);
+
   return (
     <>
-        <div>Dashboard Page</div>
-        <SignOutBtn />
+      <div>Dashboard Page</div>
+      <SignOutBtn />
     </>
-  ) 
-}
+  );
+};
 
-export default Page
+export default Page;
