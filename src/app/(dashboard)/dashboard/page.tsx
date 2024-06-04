1527db47aab7b4e7f88af9d49cd944a35a67c016
Satoki <satokiito@gmail.com>
2024-06-04 17:30:19 +0100

    feat: add guard clause to dashboard page if no user session

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import SignOutBtn from '@/components/SignOutBtn'
 import { authOptions } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
 import { FC } from 'react'
 
 interface PageProps {
@@ -10,6 +11,11 @@ interface PageProps {
 
 const Page = async ({}) => {
     const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return notFound()
+    }
+    
   return (
     <>
         <div>Dashboard Page</div>
