d709d43d4f9f085b6715b1cf6aa647f988c5e062
Satoki <satokiito@gmail.com>
2024-06-04 17:48:23 +0100

    refactor: remove dead code from dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,13 +2,9 @@ import SignOutBtn from "@/components/SignOutBtn";
 import { getFriendsByUserId } from "@/helpers/get-friends-by-userId";
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
-import { db } from "@/lib/db";
 import { chatHrefConstructor } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
-import { FC } from "react";
-
-interface PageProps {}
 
 const Page = async ({}) => {
   const session = await getServerSession(authOptions);
