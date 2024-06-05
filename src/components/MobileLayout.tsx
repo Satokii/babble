1885e0c530c06853dc582dd848244ab7325037ec
Satoki <satokiito@gmail.com>
2024-06-05 13:46:39 +0100

    feat: add Menu title

--- src/components/MobileLayout.tsx
+++ src/components/MobileLayout.tsx
@@ -94,7 +94,7 @@ const MobileLayout: FC<MobileLayoutProps> = ({
                     <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                       <div className="px-4 sm:px-6">
                         <DialogTitle className="text-base font-semibold leading-6 text-gray-900">
-                          Panel title
+                          Menu
                         </DialogTitle>
                       </div>
                       <div className="relative mt-6 flex-1 px-4 sm:px-6">
