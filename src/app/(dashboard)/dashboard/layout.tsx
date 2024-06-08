e0a69f0b0608e1fe2a5f749235e586b2087df40b
Satoki <satokiito@gmail.com>
2024-06-08 19:57:18 +0100

    refactor: update colour styling of sidebar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -59,7 +59,7 @@ const Layout = async ({ children }: LayoutProps) => {
             className="flex items-center justify-center h-16 gap-2 transition duration-300 ease-in-out hover:bg-gray-50 hover:text-gray-700 rounded-lg hover:shadow-md"
           >
             <Icons.Logo className="h-6 w-auto" />
-            <h1 className="text-xl font-bold">Time2Chat</h1>
+            <h1 className="text-xl font-bold text-cyan-500">Time2Chat</h1>
           </Link>
         </div>
 
@@ -85,9 +85,9 @@ const Layout = async ({ children }: LayoutProps) => {
                     <li key={option.id}>
                       <Link
                         href={option.href}
-                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        className="text-gray-700 hover:text-cyan-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
                       >
-                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                        <span className="text-gray-400 border-gray-200 group-hover:border-cyan-600 group-hover:text-cyan-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
                           <Icon className="h-4 w-4" />
                         </span>
 
@@ -106,7 +106,7 @@ const Layout = async ({ children }: LayoutProps) => {
             </li>
 
             <li className="mt-auto flex items-center">
-              <div className="flex flex-1 items-center gap-x-3 px-1 py-3 text-sm font-semibold leading-6 text-gray-900">
+              <div className="flex flex-1 items-center gap-x-3 px-1 py-3 text-sm font-semibold leading-5 text-gray-900">
                 <div className="relative h-8 w-8 bg-gray-50">
                   <Image
                     fill
@@ -119,14 +119,14 @@ const Layout = async ({ children }: LayoutProps) => {
 
                 <span className="sr-only">Profile</span>
                 <div className="flex flex-col">
-                  <span aria-hidden="true">{session.user.name}</span>
-                  <span className="text-xs text-zinc-400" aria-hidden="true">
+                  <span aria-hidden="true"  className="text-cyan-500">{session.user.name}</span>
+                  <span className="text-xs text-cyan-400" aria-hidden="true">
                     {session.user.email}
                   </span>
                 </div>
               </div>
 
-              <SignOutBtn className="h-full aspect-square hover:bg-gray-100" />
+              <SignOutBtn className="h-full aspect-square hover:bg-cyan-100" />
             </li>
           </ul>
         </nav>
--- src/components/FriendRequestMenu.tsx
+++ src/components/FriendRequestMenu.tsx
@@ -47,15 +47,15 @@ const FriendRequestMenu: FC<FriendRequestMenuProps> = ({
   return (
     <Link
       href="/dashboard/friend-requests"
-      className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+      className="text-gray-700 hover:text-cyan-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
     >
-      <div className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+      <div className="text-gray-400 border-gray-200 group-hover:border-cyan-600 group-hover:text-cyan-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
         <User className="h-4 w-4" />
       </div>
       <p className="truncate">Friend Requests</p>
 
       {requestCount > 0 ? (
-        <div className="rounded-full w-5 h-5 text-xs flex justify-center items-center text-white bg-indigo-600">
+        <div className="rounded-full w-5 h-5 text-xs flex justify-center items-center text-white bg-cyan-600">
           {requestCount}
         </div>
       ) : null}
