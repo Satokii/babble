b83cb5c91386a084224c4d3249c3e7fad2db2ea7
Satoki <satokiito@gmail.com>
2024-06-09 18:18:16 +0100

    refactor: update colour of reusable button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,12 +4,12 @@ import { Loader2 } from "lucide-react";
 import { ButtonHTMLAttributes, FC } from "react";
 
 export const buttonVariants = cva(
-  "active: scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+  "active: scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
   {
     variants: {
       variant: {
-        default: "bg-slate-900 text-white hover:bg-slate-800",
-        ghost: "bg-transparent hover:text-slate-900 hover-bg-slate-200",
+        default: "bg-cyan-500 text-white hover:bg-cyan-700",
+        ghost: "bg-transparent hover:text-cyan-900 hover-bg-cyan-200",
       },
       size: {
         default: "h-10 py-2 px-4",
