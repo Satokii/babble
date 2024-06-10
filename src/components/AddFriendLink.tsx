a9f2db031ae2f3a74bac2f0e9c9f1a4979c9004f
Satoki <satokiito@gmail.com>
2024-06-10 22:59:32 +0100

    feat: initialise AddFriendLink component

---
+++ src/components/AddFriendLink.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface AddFriendLinkProps {
+  
+}
+
+const AddFriendLink: FC<AddFriendLinkProps> = ({}) => {
+  return <div>AddFriendLink</div>
+}
+
+export default AddFriendLink
