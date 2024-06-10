dd31a2dd502d56c8c98c429e7e881d53c0656f1b
Satoki <satokiito@gmail.com>
2024-06-10 23:09:26 +0100

    feat: import AddFriendLink component into mobile layout

--- src/components/MobileLayout.tsx
+++ src/components/MobileLayout.tsx
@@ -19,6 +19,7 @@ import FriendRequestLink from "./FriendRequestLink";
 import Image from "next/image";
 import SignOutBtn from "./SignOutBtn";
 import { usePathname } from "next/navigation";
+import AddFriendLink from "./AddFriendLink";
 
 interface MobileLayoutProps {
   friends: User[];
@@ -138,24 +139,9 @@ const MobileLayout: FC<MobileLayoutProps> = ({
                               </div>
 
                               <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                {sidebarOptions.map((option) => {
-                                  const Icon = Icons[option.icon];
-                                  return (
-                                    <li key={option.name}>
-                                      <Link
-                                        href={option.href}
-                                        className="text-gray-700 hover:text-cyan-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                      >
-                                        <span className="text-gray-400 border-gray-200 group-hover:border-cyan-600 group-hover:text-cyan-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
-                                          <Icon className="h-4 w-4" />
-                                        </span>
-                                        <span className="truncate">
-                                          {option.name}
-                                        </span>
-                                      </Link>
-                                    </li>
-                                  );
-                                })}
+                                <li>
+                                  <AddFriendLink />
+                                </li>
 
                                 <li>
                                   <FriendRequestLink
