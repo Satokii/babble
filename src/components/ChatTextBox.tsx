ce3ba424869187e47841539ebb26c29e752a74aa
Satoki <satokiito@gmail.com>
2024-06-10 18:56:48 +0100

    feat: ChatTextBox now responsive for smaller screens

--- src/components/ChatTextBox.tsx
+++ src/components/ChatTextBox.tsx
@@ -34,10 +34,10 @@ const ChatTextBox: FC<ChatTextBoxProps> = ({ chatFriend, chatId }) => {
   };
 
   return (
-    <div className="px-4 py-4 bg-gray-50 shadow-md rounded-lg flex items-center space-x-3">
+    <div className="px-2 py-2 sm:px-4 sm:py-4 bg-gray-50 shadow-md rounded-lg flex items-center space-x-3">
       <div className="relative flex-grow">
         <TextareaAutosize
-          className="block w-full resize-none border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:border-cyan-600 focus:ring focus:ring-cyan-300 focus:ring-opacity-50 sm:py-2 sm:text-sm sm:leading-6 shadow-sm"
+          className="block w-full resize-none border border-gray-300 rounded-lg bg-white text-gray-900 placeholder-gray-500 focus:border-cyan-600 focus:ring focus:ring-cyan-300 focus:ring-opacity-50 sm:py-2 text-[0.7rem] sm:text-sm sm:leading-6 shadow-sm"
           ref={textareaRef}
           onKeyDown={(e) => {
             if (e.key === "Enter" && !e.shiftKey) {
@@ -48,14 +48,14 @@ const ChatTextBox: FC<ChatTextBoxProps> = ({ chatFriend, chatId }) => {
           rows={1}
           value={messageContent}
           onChange={(e) => setMessageContent(e.target.value)}
-          placeholder={`Send a message to ${chatFriend.name}`}
+          placeholder={`Message ${chatFriend.name}`}
         />
       </div>
       <Button
         onClick={sendMessage}
         isLoading={isLoading}
         type="submit"
-        className="bg-cyan-600 text-white rounded-lg px-4 py-2 hover:bg-cyan-500 focus:ring-2 focus:ring-cyan-600 focus:ring-opacity-50 transition duration-300"
+        className="bg-cyan-600 text-white text-[0.75rem] sm:text-sm rounded-lg px-4 py-2 hover:bg-cyan-500 focus:ring-2 focus:ring-cyan-600 focus:ring-opacity-50 transition duration-300"
       >
         Send
       </Button>
