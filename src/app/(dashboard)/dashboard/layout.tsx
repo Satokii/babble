a1ee71261e0c566fd974c2763d65e876a762e12a
Satoki <satokiito@gmail.com>
2024-06-10 21:44:45 +0100

    feat: add further styling to dashboard sidebar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -52,47 +52,46 @@ const Layout = async ({ children }: LayoutProps) => {
           friendReqCount={requestCount}
         />
       </div>
-      <div className="hidden md:flex flex-col h-full w-full max-w-[16rem] gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-3">
-        <div className="max-w-xxs">
+      <div className="hidden md:flex flex-col h-full w-full max-w-[16rem] gap-y-6 rounded-lg overflow-y-auto bg-gradient-to-r from-cyan-200 to-teal-300 border-r shadow-lg p-4">
+        <div className="max-w-xs">
           <Link
             href="/dashboard"
-            className="flex items-center justify-center h-16 gap-1 transition duration-300 ease-in-out hover:bg-gray-50 hover:text-gray-700 rounded-lg hover:shadow-md"
+            className="flex items-center justify-center h-16 gap-2 transition duration-300 ease-in-out hover:bg-cyan-100 hover:text-cyan-700 rounded-lg hover:shadow-md"
           >
-            <div className="relative w-11 h-11">
+            <div className="relative w-12 h-12">
               <Icons.Logo />
             </div>
-            <h1 className="text-xl font-bold text-cyan-500">Time2Chat</h1>
+            <h1 className="text-2xl font-bold text-cyan-600">Time2Chat</h1>
           </Link>
         </div>
 
         {friends.length > 0 ? (
-          <div className="text-xs font-semibold leading-6 text-gray-400">
+          <div className="text-sm font-semibold leading-6 text-gray-500">
             Chats
           </div>
         ) : null}
 
         <nav className="flex flex-1 flex-col flex-shrink-0">
-          <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <ul role="list" className="flex flex-1 flex-col gap-y-6">
             <li>
               <SidebarChatList friends={friends} sessionId={session.user.id} />
             </li>
             <li>
-              <div className="text-xs font-semibold leading-6 text-gray-400">
+              <div className="text-sm font-semibold leading-6 text-gray-500">
                 Overview
               </div>
-              <ul role="list" className="-mx-2 mt-2 space-y-1">
+              <ul role="list" className="-mx-2 mt-2 space-y-2">
                 {sidebarOptions.map((option) => {
                   const Icon = Icons[option.icon];
                   return (
                     <li key={option.id}>
                       <Link
                         href={option.href}
-                        className="text-gray-700 hover:text-cyan-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        className="text-gray-800 hover:text-cyan-600 hover:bg-cyan-50 group flex gap-4 rounded-md p-2 text-sm leading-6 font-semibold transition duration-300 ease-in-out"
                       >
-                        <span className="text-gray-400 border-gray-200 group-hover:border-cyan-600 group-hover:text-cyan-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                        <span className="text-gray-400 border-gray-200 group-hover:border-cyan-600 group-hover:text-cyan-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white transition duration-300 ease-in-out">
                           <Icon className="h-4 w-4" />
                         </span>
-
                         <span className="truncate">{option.name}</span>
                       </Link>
                     </li>
@@ -109,7 +108,7 @@ const Layout = async ({ children }: LayoutProps) => {
 
             <li className="mt-auto flex items-center">
               <div className="flex flex-1 items-center gap-x-3 px-1 py-3 text-sm font-semibold leading-5 text-gray-900">
-                <div className="relative h-8 w-8 bg-gray-50">
+                <div className="relative h-9 w-9">
                   <Image
                     fill
                     referrerPolicy="no-referrer"
@@ -122,13 +121,13 @@ const Layout = async ({ children }: LayoutProps) => {
                 <span className="sr-only">Profile</span>
                 <div className="flex flex-col">
                   <span aria-hidden="true">{session.user.name}</span>
-                  <span className="text-xs text-zinc-400" aria-hidden="true">
+                  <span className="text-xs text-gray-500" aria-hidden="true">
                     {session.user.email}
                   </span>
                 </div>
               </div>
 
-              <SignOutBtn className="h-full aspect-square hover:bg-cyan-100" />
+              <SignOutBtn className="h-full aspect-square hover:bg-cyan-100 transition duration-300 ease-in-out" />
             </li>
           </ul>
         </nav>
