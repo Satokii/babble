162b695c3b3005939312175bad813e1bfeede3c8
Satoki <satokiito@gmail.com>
2024-06-10 17:33:57 +0100

    refactor: add truncate to message preview

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { getFriendsByUserId } from "@/helpers/get-friends-by-userId";
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { chatHrefConstructor } from "@/lib/utils";
-import { ChevronRight } from "lucide-react";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
@@ -38,58 +37,55 @@ const Page = async ({}) => {
   return (
     <>
       <div className="container mx-auto px-4 py-8 sm:py-12 bg-gradient-to-r from-cyan-200 to-teal-300 rounded-xl shadow-lg">
-    <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl mb-8 text-center sm:text-left text-gray-800">
-      Recent Chats
-    </h1>
-    {friendsLastSentMessage.length === 0 ? (
-      <p className="text-sm text-gray-600 text-center sm:text-left">
-        Nothing to see here
-      </p>
-    ) : (
-      friendsLastSentMessage.map((friend) => (
-        <div
-          key={friend.id}
-          className="relative bg-white border border-gray-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow mb-4"
-        >
-          <div className="absolute right-4 top-1/2 transform -translate-y-1/2">
-            <ChevronRight className="h-6 w-6 text-gray-400" />
-          </div>
-          <Link
-            href={`/dashboard/chat/${chatHrefConstructor(
-              session.user.id,
-              friend.id
-            )}`}
-            className="relative flex items-center space-x-3 sm:space-x-4"
-          >
-            <div className="flex-shrink-0">
-              <div className="relative h-12 w-12 rounded-full overflow-hidden bg-cyan-200">
-                <Image
-                  fill
-                  referrerPolicy="no-referrer"
-                  className="rounded-full"
-                  alt={`${friend.name} profile photo`}
-                  src={friend.image}
-                />
-              </div>
+        <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl mb-8 text-center sm:text-left text-gray-800">
+          Recent Chats
+        </h1>
+        {friendsLastSentMessage.length === 0 ? (
+          <p className="text-sm text-gray-600 text-center sm:text-left">
+            Nothing to see here
+          </p>
+        ) : (
+          friendsLastSentMessage.map((friend) => (
+            <div
+              key={friend.id}
+              className="relative bg-white border border-gray-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow mb-4"
+            >
+              <Link
+                href={`/dashboard/chat/${chatHrefConstructor(
+                  session.user.id,
+                  friend.id
+                )}`}
+                className="relative flex items-center space-x-3 sm:space-x-4"
+              >
+                <div className="flex-shrink-0">
+                  <div className="relative h-12 w-12 rounded-full overflow-hidden bg-cyan-200">
+                    <Image
+                      fill
+                      referrerPolicy="no-referrer"
+                      className="rounded-full"
+                      alt={`${friend.name} profile photo`}
+                      src={friend.image}
+                    />
+                  </div>
+                </div>
+                <div className="truncate">
+                  <h4 className="text-base sm:text-lg font-semibold text-cyan-700">
+                    {friend.name}
+                  </h4>
+                  <p className="mt-1 text-sm sm:text-base text-gray-700">
+                    <span className="font-semibold">
+                      {friend.lastMessage.senderId === session.user.id
+                        ? "You: "
+                        : ""}
+                    </span>
+                    <span>{friend.lastMessage.text}</span>
+                  </p>
+                </div>
+              </Link>
             </div>
-            <div>
-              <h4 className="text-base sm:text-lg font-semibold text-cyan-700">
-                {friend.name}
-              </h4>
-              <p className="mt-1 text-sm sm:text-base text-gray-700">
-                <span className="font-semibold">
-                  {friend.lastMessage.senderId === session.user.id
-                    ? "You: "
-                    : ""}
-                </span>
-                <span>{friend.lastMessage.text}</span>
-              </p>
-            </div>
-          </Link>
-        </div>
-      ))
-    )}
-  </div>
+          ))
+        )}
+      </div>
     </>
   );
 };
