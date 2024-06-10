ecd84a9852cb7dc150c5638f9e9cd2d898bcb602
Satoki <satokiito@gmail.com>
2024-06-10 23:22:21 +0100

    refactor: update styling of AddFriendLink

--- src/components/AddFriendLink.tsx
+++ src/components/AddFriendLink.tsx
@@ -5,12 +5,12 @@ const AddFriendLink = () => {
   return (
     <Link
       href="/dashboard/add"
-      className="text-gray-800 hover:text-cyan-600 hover:bg-cyan-50 group flex gap-4 rounded-md p-2 text-sm leading-6 font-semibold transition duration-300 ease-in-out"
+      className="group flex items-center gap-x-3 rounded-md p-2 text-sm font-semibold text-gray-700 transition duration-300 ease-in-out hover:bg-cyan-500 hover:text-white"
     >
-      <span className="text-gray-400 border-gray-200 group-hover:border-cyan-600 group-hover:text-cyan-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white transition duration-300 ease-in-out">
+      <div className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-200 bg-white text-[0.625rem] font-medium text-gray-400 transition duration-300 ease-in-out group-hover:border-cyan-600 group-hover:text-cyan-600">
         <Icons.UserPlus className="h-4 w-4" />
-      </span>
-      <span className="truncate">Add Friend</span>
+      </div>
+      <p className="truncate">Add Friend</p>
     </Link>
   );
 };
