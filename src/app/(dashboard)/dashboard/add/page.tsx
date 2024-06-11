5a154c3ea8d9bd153201de2afacbbf3b48321eaa
Satoki <satokiito@gmail.com>
2024-06-11 02:31:00 +0100

    refactor: remove testing code

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,9 +1,7 @@
 import AddFriendForm from "@/components/AddFriendForm";
 import { FC } from "react";
 
-const page: FC = async ({}) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-
+const page: FC = ({}) => {
   return (
     <main className="container mx-auto px-4 py-8 sm:py-12 bg-gradient-to-r from-cyan-200 to-teal-300 rounded-xl shadow-lg">
       <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl mb-8 text-center sm:text-left text-gray-800">
