d7e2db8cbd534422b9a74d776ce1b5881a53ca5e
Satoki <satokiito@gmail.com>
2024-06-11 02:36:24 +0100

    feat: add details to friend request loading page

--- src/app/(dashboard)/dashboard/friend-requests/loading.tsx
+++ src/app/(dashboard)/dashboard/friend-requests/loading.tsx
@@ -1,18 +1,15 @@
-import { FC } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-interface loadingProps {}
-
-const loading: FC<loadingProps> = ({}) => {
+const loading = () => {
   return (
     <main className="container mx-auto px-4 py-8 sm:py-12 rounded-xl shadow-lg">
       <h1 className="mb-8">
-        <Skeleton height={60} width={350} />
+        <Skeleton height={60} width={230} />
       </h1>
       <div className="flex flex-col gap-4">
-        <div className="flex px-2 gap-2 sm:gap-5 items-center justify-start">
-          <Skeleton height={40} width={400} />
+        <div className="flex gap-1 sm:gap-5 items-center justify-start">
+          <Skeleton height={40} width={180} />
           <div className="place-items-center">
             <Skeleton height={30} width={30} circle />
           </div>
@@ -20,8 +17,8 @@ const loading: FC<loadingProps> = ({}) => {
             <Skeleton height={30} width={30} circle />
           </div>
         </div>
-        <div className="flex px-2 gap-2 sm:gap-5 items-center justify-center sm:justify-start">
-          <Skeleton height={40} width={400} />
+        <div className="flex gap-1 sm:gap-5 items-center justify-start">
+          <Skeleton height={40} width={180} />
           <div className="place-items-center">
             <Skeleton height={30} width={30} circle />
           </div>
