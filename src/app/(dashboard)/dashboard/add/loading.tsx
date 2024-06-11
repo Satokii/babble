db4d29dde5a603145d716a72c36e0aa6cedaba1d
Satoki <satokiito@gmail.com>
2024-06-11 02:17:26 +0100

    feat: add details to add friend loading page

--- src/app/(dashboard)/dashboard/add/loading.tsx
+++ src/app/(dashboard)/dashboard/add/loading.tsx
@@ -1,16 +1,21 @@
-import { FC } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-interface loadingProps {}
-
-const loading: FC<loadingProps> = ({}) => {
+const loading = () => {
   return (
-    <div className="w-full flex flex-col gap-3">
-      <Skeleton className="mb-4" height={60} width={500} />
-      <Skeleton height={20} width={150} />
-      <Skeleton height={50} width={400} />
-    </div>
+    <main className="container mx-auto px-4 py-8 sm:py-12 rounded-xl shadow-lg">
+      <h1>
+        <Skeleton className="mb-4" height={60} width={250} />
+      </h1>
+      <form className="max-w-sm w-full mx-auto p-6 bg-white border border-gray-300 shadow-md rounded-lg">
+        <div>
+          <Skeleton height={25} width={150} />
+        </div>
+        <div className="mt-4">
+          <Skeleton height={40} width={330} />
+        </div>
+      </form>
+    </main>
   );
 };
 
