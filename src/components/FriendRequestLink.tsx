32fbb834d3fdf5d5e3cbc545fce1fba941697413
Satoki <satokiito@gmail.com>
2024-06-11 01:32:49 +0100

    refactor: define height of all sidebar links for continuity

--- src/components/FriendRequestLink.tsx
+++ src/components/FriendRequestLink.tsx
@@ -47,7 +47,7 @@ const FriendRequestMenu: FC<FriendRequestMenuProps> = ({
   return (
     <Link
       href="/dashboard/friend-requests"
-      className="group flex items-center gap-x-3 rounded-md p-2 text-sm font-semibold text-gray-700 transition duration-300 ease-in-out hover:bg-cyan-500 hover:text-white"
+      className="group flex items-center h-10 gap-x-3 rounded-md p-2 text-sm font-semibold text-gray-700 transition duration-300 ease-in-out hover:bg-cyan-500 hover:text-white"
     >
       <div className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-200 bg-white text-[0.625rem] font-medium text-gray-400 transition duration-300 ease-in-out group-hover:border-cyan-600 group-hover:text-cyan-600">
         <User className="h-4 w-4" />
