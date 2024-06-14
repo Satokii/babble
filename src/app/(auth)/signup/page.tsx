a8b02af5a9855fa97e899850c6be9f54dff3a56a
Satoki <satokiito@gmail.com>
2024-06-14 14:36:17 +0100

    feat: add extra error handling to signup to display error if user already exists

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/ui/Button";
 import toast from "react-hot-toast";
 import LogoImage from "@/public/logo.png";
 import Image from "next/image";
-import axios from "axios";
+import axios, { Axios, AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
@@ -16,6 +16,7 @@ const Page: FC = () => {
   const [emailSignup, setEmailSignup] = useState("");
   const [nameSignup, setNameSignup] = useState("");
   const [passwordSignup, setPasswordSignup] = useState("");
+  const [signupError, setSignupError] = useState("")
 
   const handleSignup = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
@@ -39,7 +40,12 @@ const Page: FC = () => {
       router.push("/login");
       toast.success("Sign up successful. Please log in.");
     } catch (err) {
-      toast.error("Sign up failed. Please try again.");
+        if (err instanceof AxiosError) {
+            toast.error(err.response?.data.message)
+        }
+        else {
+            toast.error("Sign up failed. Please try again.");
+        }
     } finally {
       setIsLoadingSignup(false);
     }
