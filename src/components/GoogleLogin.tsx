d7271aaef5f985166bbddf8be4e8f48d329f5413
Satoki <satokiito@gmail.com>
2024-06-14 15:48:46 +0100

    feat: initialise GoogleLogin component

---
+++ src/components/GoogleLogin.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface GoogleLoginProps {
+  
+}
+
+const GoogleLogin: FC<GoogleLoginProps> = ({}) => {
+  return <div>GoogleLogin</div>
+}
+
+export default GoogleLogin
