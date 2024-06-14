bb17cdcefb46de7e72a6745560163fc4f1c7f234
Satoki <satokiito@gmail.com>
2024-06-14 15:42:28 +0100

    refactor: remove login form from login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,38 +6,11 @@ import { signIn } from "next-auth/react";
 import toast from "react-hot-toast";
 import LogoImage from "@/public/logo.png";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
+import LoginForm from "@/components/LoginForm";
 
 const Page: FC = () => {
-  const router = useRouter();
   const [isLoadingGoogle, setIsLoadingGoogle] = useState(false);
-  const [isLoadingLogin, setIsLoadingLogin] = useState(false);
-
-  const [emailLogin, setEmailLogin] = useState("");
-  const [passwordLogin, setPasswordLogin] = useState("");
-
-  const handleLogin = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    setIsLoadingLogin(true);
-    try {
-      const result = await signIn("credentials", {
-        redirect: false,
-        email: emailLogin,
-        password: passwordLogin,
-      });
-
-      if (result?.ok) {
-        router.push("/dashboard");
-      } else {
-        toast.error("Login failed. Email or password are incorrect.");
-      }
-    } catch (err) {
-      toast.error("An error occurred. Please try again.");
-    } finally {
-      setIsLoadingLogin(false);
-    }
-  };
 
   const loginWithGoogle = async () => {
     setIsLoadingGoogle(true);
@@ -72,28 +45,7 @@ const Page: FC = () => {
               Sign up.
             </Link>
           </div>
-          <form
-            onSubmit={handleLogin}
-            className="space-y-4 max-w-md mx-auto p-4 border rounded-lg shadow-md"
-          >
-            <input
-              type="email"
-              placeholder="Email"
-              value={emailLogin}
-              onChange={(e) => setEmailLogin(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-cyan-500"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={passwordLogin}
-              onChange={(e) => setPasswordLogin(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-cyan-500"
-            />
-            <Button className="w-full" isLoading={isLoadingLogin} type="submit">
-              Sign In
-            </Button>
-          </form>
+          <LoginForm />
           <Button
             isLoading={isLoadingGoogle}
             type="button"
