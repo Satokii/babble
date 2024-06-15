ea10d0aad697b1875fa19d9185fb3fa3f90a767b
Satoki <satokiito@gmail.com>
2024-06-15 23:58:09 +0100

    feat: initialise ProfileDetails component

---
+++ src/components/ProfileDetails.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ProfileDetailsProps {
+  
+}
+
+const ProfileDetails: FC<ProfileDetailsProps> = ({}) => {
+  return <div>ProfileDetails</div>
+}
+
+export default ProfileDetails
