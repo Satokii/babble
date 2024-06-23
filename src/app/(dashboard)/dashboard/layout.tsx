ba60199aeb36ae3cf87d0c619eb3816fb5a89dae
Satoki <satokiito@gmail.com>
2024-06-23 22:24:33 +0100

    refactor: import ProfileLink component into dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -12,6 +12,7 @@ import { getFriendsByUserId } from "@/helpers/get-friends-by-userId";
 import SidebarChatList from "@/components/SidebarChatList";
 import MobileLayout from "@/components/MobileLayout";
 import AddFriendLink from "@/components/AddFriendLink";
+import ProfileLink from "@/components/ProfileLink";
 
 interface LayoutProps {
   children: ReactNode;
@@ -84,29 +85,7 @@ const Layout = async ({ children }: LayoutProps) => {
             </li>
 
             <li className="mt-auto flex items-center">
-              <Link
-                href="/dashboard/profile"
-                className="flex flex-1 items-center gap-x-3 px-1 py-2 text-sm font-semibold leading-5 text-gray-900 transition rounded-md duration-300 ease-in-out hover:bg-cyan-500 hover:text-white"
-              >
-                <div className="relative h-9 w-9">
-                  <Image
-                    fill
-                    referrerPolicy="no-referrer"
-                    className="rounded-full"
-                    src={session?.user.image || ""}
-                    alt="User profile picture"
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  />
-                </div>
-
-                <span className="sr-only">Profile</span>
-                <div className="flex flex-col">
-                  <span aria-hidden="true">{session.user.name}</span>
-                  <span className="text-xs text-gray-500" aria-hidden="true">
-                    {session.user.email}
-                  </span>
-                </div>
-              </Link>
+              <ProfileLink session={session} />
               <SignOutBtn />
             </li>
           </ul>
--- src/components/ProfileLink.tsx
+++ src/components/ProfileLink.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const ProfileLink = ({}) => {
+const ProfileLink = ({ session }) => {
   return (
     <Link
       href="/dashboard/profile"
