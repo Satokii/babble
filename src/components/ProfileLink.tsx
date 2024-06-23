d188a80f517bb65e9a42413b721797cf4dfa4b55
Satoki <satokiito@gmail.com>
2024-06-23 22:21:26 +0100

    feat: initialise ProfileLink component

---
+++ src/components/ProfileLink.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ProfileLinkProps {
+  
+}
+
+const ProfileLink: FC<ProfileLinkProps> = ({}) => {
+  return <div>ProfileLink</div>
+}
+
+export default ProfileLink
