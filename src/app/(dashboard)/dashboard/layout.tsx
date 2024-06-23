b5edfa364304f045911f28aa8702e5c06197042b
Satoki <satokiito@gmail.com>
2024-06-23 22:20:40 +0100

    refactor: refactor code to replace div with link for profile link

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -86,27 +86,25 @@ const Layout = async ({ children }: LayoutProps) => {
             <li className="mt-auto flex items-center">
               <Link
                 href="/dashboard/profile"
-                className="transition rounded-md duration-300 ease-in-out hover:bg-cyan-500"
+                className="flex flex-1 items-center gap-x-3 px-1 py-2 text-sm font-semibold leading-5 text-gray-900 transition rounded-md duration-300 ease-in-out hover:bg-cyan-500 hover:text-white"
               >
-                <div className="flex flex-1 items-center gap-x-3 px-1 py-2 text-sm font-semibold leading-5 text-gray-900">
-                  <div className="relative h-9 w-9">
-                    <Image
-                      fill
-                      referrerPolicy="no-referrer"
-                      className="rounded-full"
-                      src={session?.user.image || ""}
-                      alt="User profile picture"
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    />
-                  </div>
+                <div className="relative h-9 w-9">
+                  <Image
+                    fill
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
+                    src={session?.user.image || ""}
+                    alt="User profile picture"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                </div>
 
-                  <span className="sr-only">Profile</span>
-                  <div className="flex flex-col">
-                    <span aria-hidden="true">{session.user.name}</span>
-                    <span className="text-xs text-gray-500" aria-hidden="true">
-                      {session.user.email}
-                    </span>
-                  </div>
+                <span className="sr-only">Profile</span>
+                <div className="flex flex-col">
+                  <span aria-hidden="true">{session.user.name}</span>
+                  <span className="text-xs text-gray-500" aria-hidden="true">
+                    {session.user.email}
+                  </span>
                 </div>
               </Link>
               <SignOutBtn />
