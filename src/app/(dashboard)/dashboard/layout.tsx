b280816d8b789dfa7bd56d434241631af0befad8
Satoki <satokiito@gmail.com>
2024-06-24 23:03:36 +0100

    feat: add styling to logout text

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -89,8 +89,7 @@ const Layout = async ({ children }: LayoutProps) => {
               <SignOutBtn />
             </li>
           </ul>
-          <div>logout</div>
-
+          <div className="flex text-sm items-center justify-end">Log Out</div>
         </nav>
       </div>
       <section className="max-h-screen container my-6 sm:my-2 py-16 md:py-12 w-full">
