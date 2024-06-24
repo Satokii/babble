361473de44f4edff773ed7025fe6d26bdc3845d9
Satoki <satokiito@gmail.com>
2024-06-24 23:41:14 +0100

    feat: add shadow hover effect to profile link

--- src/components/ProfileLink.tsx
+++ src/components/ProfileLink.tsx
@@ -11,7 +11,7 @@ const ProfileLink: FC<ProfileLinkProps> = ({ session }) => {
   return (
     <Link
       href="/dashboard/profile"
-      className="flex flex-1 items-center gap-x-3 px-2 py-2 text-sm font-semibold leading-5 text-gray-900 transition rounded-md duration-300 ease-in-out hover:bg-cyan-100 hover:text-cyan-700"
+      className="flex flex-1 items-center gap-x-3 px-2 py-2 text-sm font-semibold leading-5 text-gray-900 transition rounded-md duration-300 ease-in-out hover:bg-cyan-100 hover:text-cyan-700 hover:shadow-md"
     >
       <div className="relative h-9 w-9">
         <Image
