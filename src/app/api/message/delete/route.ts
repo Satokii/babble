1324cdb6aa91e4ca038609ec4081942c320c314d
Satoki <satokiito@gmail.com>
2024-06-26 20:01:07 +0100

    feat: add server session check to delete message api route

--- src/app/api/message/delete/route.ts
+++ src/app/api/message/delete/route.ts
@@ -1,3 +1,13 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
 export async function POST(req: Request) {
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    
  console.log("message deleted")
 }
