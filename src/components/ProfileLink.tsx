db87106460b684bf1a0e276f8b7ab7b74de78cca
Satoki <satokiito@gmail.com>
2024-06-27 21:48:53 +0100

    refactor: update styling of profile photo

--- src/components/ProfileLink.tsx
+++ src/components/ProfileLink.tsx
@@ -11,9 +11,9 @@ const ProfileLink: FC<ProfileLinkProps> = ({ session }) => {
   return (
     <Link
       href="/dashboard/profile"
-      className="flex flex-1 items-center gap-x-3 px-2 py-2 text-sm font-semibold leading-5 text-gray-900 transition rounded-md duration-300 ease-in-out hover:bg-cyan-100 hover:text-cyan-700 hover:shadow-md"
+      className="flex items-center p-1 mr-2 transition rounded-full duration-300 ease-in-out hover:bg-cyan-100"
     >
-      <div className="relative h-9 w-9">
+      <div className="relative h-12 w-12">
         <Image
           fill
           referrerPolicy="no-referrer"
