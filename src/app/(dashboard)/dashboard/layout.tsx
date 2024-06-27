09dad816a312162f55c1a26f12b74e28054028b4
Satoki <satokiito@gmail.com>
2024-06-27 18:01:04 +0100

    refactor: dashboard layout to add header

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -35,17 +35,10 @@ const Layout = async ({ children }: LayoutProps) => {
   ).length;
 
   return (
-    <div className="w-full flex h-screen bg-gray-50">
-      <div className="md:hidden">
-        <MobileLayout
-          friends={friends}
-          session={session}
-          friendReqCount={requestCount}
-        />
-      </div>
-      <div className="hidden md:flex flex-col h-full w-full max-w-[16rem] gap-y-6 rounded-lg overflow-y-auto bg-gradient-to-r from-cyan-200 to-teal-300 border-r shadow-lg px-4 pt-4 pb-2">
-        <div className="max-w-xs">
-          <Link
+    <div className="w-full h-screen bg-gray-50 flex flex-col">
+    <header className="w-full bg-gradient-to-r from-cyan-200 to-teal-300 shadow-lg p-4 flex items-center justify-between">
+      <div className=" flexjustify-self-start">
+      <Link
             href="/dashboard"
             className="flex items-center justify-center h-16 gap-2 transition duration-300 ease-in-out hover:bg-cyan-100 hover:text-cyan-700 rounded-lg hover:shadow-md"
           >
@@ -54,6 +47,30 @@ const Layout = async ({ children }: LayoutProps) => {
             </div>
             <h1 className="text-2xl font-bold text-cyan-600">Babble</h1>
           </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <ProfileLink session={session} />
+      </div>
+    </header>
+    <div className="flex flex-1 overflow-hidden">
+      <div className="md:hidden">
+        <MobileLayout
+          friends={friends}
+          session={session}
+          friendReqCount={requestCount}
+        />
+      </div>
+      <div className="hidden md:flex flex-col h-full max-w-[16rem] gap-y-6 rounded-lg overflow-y-auto bg-gradient-to-r from-cyan-200 to-teal-300 border-r shadow-lg px-4 pt-4 pb-2">
+        <div className="max-w-xs">
+          {/* <Link
+            href="/dashboard"
+            className="flex items-center justify-center h-16 gap-2 transition duration-300 ease-in-out hover:bg-cyan-100 hover:text-cyan-700 rounded-lg hover:shadow-md"
+          >
+            <div className="relative w-12 h-12">
+              <Icons.Logo />
+            </div>
+            <h1 className="text-2xl font-bold text-cyan-600">Babble</h1>
+          </Link> */}
         </div>
 
         {friends.length > 0 ? (
@@ -93,10 +110,73 @@ const Layout = async ({ children }: LayoutProps) => {
           </div>
         </nav>
       </div>
-      <section className="max-h-screen container my-6 sm:my-2 py-16 md:py-12 w-full">
+      <section className="max-h-screen container my-6 sm:my-2 py-16 md:py-12 w-full overflow-y-auto">
         {children}
       </section>
     </div>
+  </div>
+    // <div className="w-full flex h-screen bg-gray-50">
+    //   <div className="md:hidden">
+    //     <MobileLayout
+    //       friends={friends}
+    //       session={session}
+    //       friendReqCount={requestCount}
+    //     />
+    //   </div>
+    //   <div className="hidden md:flex flex-col h-full w-full max-w-[16rem] gap-y-6 rounded-lg overflow-y-auto bg-gradient-to-r from-cyan-200 to-teal-300 border-r shadow-lg px-4 pt-4 pb-2">
+    //     <div className="max-w-xs">
+    //       <Link
+    //         href="/dashboard"
+    //         className="flex items-center justify-center h-16 gap-2 transition duration-300 ease-in-out hover:bg-cyan-100 hover:text-cyan-700 rounded-lg hover:shadow-md"
+    //       >
+    //         <div className="relative w-12 h-12">
+    //           <Icons.Logo />
+    //         </div>
+    //         <h1 className="text-2xl font-bold text-cyan-600">Babble</h1>
+    //       </Link>
+    //     </div>
+
+    //     {friends.length > 0 ? (
+    //       <div className="text-sm font-semibold leading-6 text-gray-500">
+    //         Chats
+    //       </div>
+    //     ) : null}
+
+    //     <nav className="flex flex-1 flex-col flex-shrink-0">
+    //       <ul role="list" className="flex flex-1 flex-col gap-y-6">
+    //         <li>
+    //           <SidebarChatList friends={friends} sessionId={session.user.id} />
+    //         </li>
+    //         <li>
+    //           <div className="text-sm font-semibold leading-6 text-gray-500">
+    //             Overview
+    //           </div>
+    //           <ul role="list" className="-mx-2 mt-2 space-y-2">
+    //             <li>
+    //               <AddFriendLink />
+    //             </li>
+    //             <li>
+    //               <FriendRequestLink
+    //                 sessionId={session.user.id}
+    //                 INITIAL_COUNT={requestCount}
+    //               />
+    //             </li>
+    //           </ul>
+    //         </li>
+
+    //         <li className="mt-auto flex items-center">
+    //           <ProfileLink session={session} />
+    //         </li>
+    //       </ul>
+    //       <div className="flex items-center justify-end">
+    //         <SignOutBtn />
+    //       </div>
+    //     </nav>
+    //   </div>
+    //   <section className="max-h-screen container my-6 sm:my-2 py-16 md:py-12 w-full">
+    //     {children}
+    //   </section>
+    // </div>
   );
 };
 
