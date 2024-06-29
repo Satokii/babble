624778567e5cb936e9ba9668044a835f4ba7fba1
Satoki <satokiito@gmail.com>
2024-06-29 21:18:37 +0100

    style: formatting

--- src/components/ProfileDetails.tsx
+++ src/components/ProfileDetails.tsx
@@ -11,35 +11,34 @@ interface ProfileDetailsProps {
 const ProfileDetails: FC<ProfileDetailsProps> = ({ user, handleEdit }) => {
   return (
     <div className="space-y-8">
-  <div className="flex flex-col items-center space-y-6">
-    <Image
-      width={120}
-      height={120}
-      referrerPolicy="no-referrer"
-      className="rounded-full border-4 border-gray-200"
-      src={user.image || ""}
-      alt="User profile picture"
-      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-    />
-    <div className="text-center space-y-4">
-      <div>
-        <h2 className="text-lg font-semibold text-gray-700">Name</h2>
-        <p className="text-sm text-gray-600">{user.name}</p>
-      </div>
-      <div>
-        <h2 className="text-lg font-semibold text-gray-700">Email</h2>
-        <p className="text-sm text-gray-600">{user.email}</p>
+      <div className="flex flex-col items-center space-y-6">
+        <Image
+          width={120}
+          height={120}
+          referrerPolicy="no-referrer"
+          className="rounded-full border-4 border-gray-200"
+          src={user.image || ""}
+          alt="User profile picture"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+        <div className="text-center space-y-4">
+          <div>
+            <h2 className="text-lg font-semibold text-gray-700">Name</h2>
+            <p className="text-sm text-gray-600">{user.name}</p>
+          </div>
+          <div>
+            <h2 className="text-lg font-semibold text-gray-700">Email</h2>
+            <p className="text-sm text-gray-600">{user.email}</p>
+          </div>
+        </div>
+        <button
+          onClick={handleEdit}
+          className="py-2 px-6 border border-transparent text-base font-medium rounded-full text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+        >
+          Edit Profile
+        </button>
       </div>
     </div>
-    <button
-      onClick={handleEdit}
-      className="py-2 px-6 border border-transparent text-base font-medium rounded-full text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-    >
-      Edit Profile
-    </button>
-  </div>
-</div>
-
   );
 };
 
