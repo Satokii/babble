8797584d8d6ad78c3421a2ab1d9e8a55058e0326
Satoki <satokiito@gmail.com>
2024-06-29 21:26:24 +0100

    refactor: update hover styling of ProfileLink

--- src/components/ProfileLink.tsx
+++ src/components/ProfileLink.tsx
@@ -11,13 +11,13 @@ const ProfileLink: FC<ProfileLinkProps> = ({ session }) => {
   return (
     <Link
       href="/dashboard/profile"
-      className="flex items-center p-1 mr-2 rounded-full transition duration-300 ease-in-out hover:scale-105 hover:bg-cyan-100"
+      className="flex items-center mr-2"
     >
-      <div className="relative h-12 w-12 rounded-full border border-cyan-100">
+      <div className="relative h-12 w-12 ">
         <Image
           fill
           referrerPolicy="no-referrer"
-          className="rounded-full"
+          className="rounded-full border-2 border-cyan-100 transition duration-300 ease-in-out hover:scale-110 hover:border-cyan-200"
           src={session?.user.image || ""}
           alt="User profile picture"
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
