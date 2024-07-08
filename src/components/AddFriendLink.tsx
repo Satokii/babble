8f0f772888d7298f4335db9377f4a67b54ebefcb
Satoki <satokiito@gmail.com>
2024-07-08 17:26:46 +0100

    refactor: further refactoring of add friend image outer div

--- src/components/AddFriendLink.tsx
+++ src/components/AddFriendLink.tsx
@@ -7,9 +7,7 @@ const AddFriendLink = () => {
       href="/dashboard/add"
       className="group flex items-center h-10 gap-x-3 rounded-md p-2 text-sm font-semibold text-gray-700 transition duration-300 ease-in-out hover:bg-cyan-500 hover:text-white"
     >
-      <div className="flex h-6 w-6 shrink-0 items-center justify-center">
-        <AddFriendImg />
-      </div>
+      <AddFriendImg />
       <p className="truncate">Add Friend</p>
     </Link>
   );
