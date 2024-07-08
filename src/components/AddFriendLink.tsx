308bbfcbdf7592c58fbd7a79f68b0c1957015890
Satoki <satokiito@gmail.com>
2024-07-08 17:22:52 +0100

    refactor: remove redundant styling from AddFriendLink

--- src/components/AddFriendLink.tsx
+++ src/components/AddFriendLink.tsx
@@ -7,7 +7,7 @@ const AddFriendLink = () => {
       href="/dashboard/add"
       className="group flex items-center h-10 gap-x-3 rounded-md p-2 text-sm font-semibold text-gray-700 transition duration-300 ease-in-out hover:bg-cyan-500 hover:text-white"
     >
-      <div className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg text-[0.625rem] font-medium text-gray-400 transition duration-300 ease-in-out group-hover:border-cyan-600 group-hover:text-cyan-600">
+      <div className="flex h-6 w-6 shrink-0 items-center justify-center">
         <AddFriendImg />
       </div>
       <p className="truncate">Add Friend</p>
