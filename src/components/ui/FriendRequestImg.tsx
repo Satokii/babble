86f67f6e17ef9e0167466d1f07e0e72c44cbef09
Satoki <satokiito@gmail.com>
2024-07-08 17:28:08 +0100

    feat: initialise FriendRequestImg component

---
+++ src/components/ui/FriendRequestImg.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface FriendRequestImgProps {
+  
+}
+
+const FriendRequestImg: FC<FriendRequestImgProps> = ({}) => {
+  return <div>FriendRequestImg</div>
+}
+
+export default FriendRequestImg
